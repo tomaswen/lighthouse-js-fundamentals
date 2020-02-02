a0d1fd61a2e604ef309ca46fdb84c65f77e27f55
Tomas Wen Liang <tomas.wenn@gmail.com>
2020-02-02 15:52:57 +0000

    Added more conditionals example

--- conditionals.js
+++ conditionals.js
@@ -1,3 +1,17 @@
+const raining = true;
+const cold = false;
+
+if (raining) {
+  console.log("Don't forget your umbrella!");
+}
+
+if (cold) {
+  console.log("Make sure you pick out a scarf!");
+}
+
+console.log("Now you're ready to go outside!");
+
+
 const temperature = 20;
 
 if (temperature < 0) {
