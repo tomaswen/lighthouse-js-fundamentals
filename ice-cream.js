06a54938f8239d09bcddb51730cb9bfe64fede42
Tomas Wen Liang <tomas.wenn@gmail.com>
2020-02-03 23:23:05 +0000

    code for Arrays-Ice cream!

---
+++ ice-cream.js
@@ -0,0 +1,6 @@
+const iceCreamFlavours = ["chocolate", "vanilla", "cookies and cream", "rocky road", "strawberry"];
+console.log(iceCreamFlavours);
+iceCreamFlavours.push('root beer');
+console.log("Bob" || " Marley")
+console.log("Jeremy" > "Khurram")
+console.log("15"===15)
