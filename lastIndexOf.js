a146e5e35b6b8648dc86774c0166f7a2c8da80b9
Tomas Wen Liang <tomas.wenn@gmail.com>
2020-02-04 00:05:05 +0000

    Determines the last index of an item in the array

---
+++ lastIndexOf.js
@@ -0,0 +1,14 @@
+function lastIndexOf(array,num){
+  let location = -1;
+  for (i = 0; i < array.length; i++){
+    if (array[i] ===  num){
+      location = i;
+    }
+  }
+  return location;
+}
+console.log(lastIndexOf([ 0, 1, 4, 1, 2 ], 1), "=?", 3);
+console.log(lastIndexOf([ 0, 1, 4, 1, 2 ], 2), "=?", 4);
+console.log(lastIndexOf([ 0, 1, 4, 1, 2 ], 3), "=?", -1);
+console.log(lastIndexOf([ 5, 5, 5 ], 5), "=?", 2);
+console.log(lastIndexOf([], 3), "=?", -1);
